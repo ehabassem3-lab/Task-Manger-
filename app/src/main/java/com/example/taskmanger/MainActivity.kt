085abe5f8584a5d5170aa0abe5f4335cbb461724
ehabassem3.lab <ehabassem3@gmail.com>
2026-08-22 16:55:08 +0300

    test

--- app/src/main/java/com/example/taskmanger/MainActivity.kt
+++ app/src/main/java/com/example/taskmanger/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TaskMangerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ehab Assem",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
